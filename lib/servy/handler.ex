e35a0ea2c058f782571245ef0e973069afbf1ec6
themind1991 <themind1991@localhost.localdomain>
2020-05-02 07:01:15 -0400

    initial commit

---
+++ lib/servy/handler.ex
@@ -0,0 +1,43 @@
+defmodule Servy.Handler do
+
+  def handle(request)do
+    request
+    |> parse
+    |> route
+    |> format_response
+
+  end
+
+  def parse(request) do
+    conv = %{method: "GET", path: "/wildthings", resp_body: ""}
+  end
+
+  def route(request) do
+    conv = %{method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers"}
+  end
+
+  def format_response(request) do
+      # TODO: Use values in the map to create an HTTP response string:
+      """
+      HTTP/1.1 200 OK
+      Content-Type: text/html
+      Content-Length: 20
+
+      Bears, Lions, Tigers
+      """
+  end
+end
+
+
+request = """
+GET /wildthings HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
