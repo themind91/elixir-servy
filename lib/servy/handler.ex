ae80cc64a25d965e57528ac6737258acda85ebed
themind1991 <themind1991@localhost.localdomain>
2020-05-02 12:28:13 -0400

    updating source code

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -3,30 +3,78 @@ defmodule Servy.Handler do
   def handle(request)do
     request
     |> parse
+    |> log
     |> route
     |> format_response
 
   end
 
+  def log(conv) do
+    IO.inspect conv
+  end
+
   def parse(request) do
-    conv = %{method: "GET", path: "/wildthings", resp_body: ""}
+
+    [method , path, _] =
+      request
+      |> String.split("\n")
+      |> List.first
+      |> String.split(" ")
+      %{method: method, path: path, resp_body: "", status: ""}
   end
 
-  def route(request) do
-    conv = %{method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers"}
+  def route(conv) do
+
+    #conv = %{method: request[:method], path: request[:path], resp_body: "Bears, Lions, Tigers"}
+    #%{ conv | resp_body: "Bears, Lions, Tigers"}
+    route(conv, conv.method, conv.path)
+
   end
 
-  def format_response(request) do
+  def route(conv, "GET", "/wildthings") do
+    %{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }
+  end
+
+  def route(conv, "GET", "/bears") do
+    %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington" }
+  end
+
+  def route(conv, "GET", "/bears/"<> id) do
+    %{ conv | status: 200, resp_body: "Bear #{id} was found " }
+  end
+
+
+  def route(conv, "DELETE", "/bears/"<> id) do
+    %{ conv | status: 200, resp_body: "Bear #{id} was deleted" }
+  end
+
+  def route(conv, _method, path) do
+    %{ conv | status: 404, resp_body: " #{path} Not found"}
+  end
+
+  def format_response(response) do
       # TODO: Use values in the map to create an HTTP response string:
       """
-      HTTP/1.1 200 OK
+      HTTP/1.1 #{response.status} #{status_reason(response.status)}
       Content-Type: text/html
-      Content-Length: 20
+      Content-Length: #{String.length(response.resp_body)}
 
-      Bears, Lions, Tigers
+      #{response.resp_body}
       """
   end
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
+  end
 end
+
 
 
 request = """
@@ -37,6 +85,56 @@ Accept: */*
 
 """
 
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /bigfoot HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+
+
+request = """
+DELETE /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+
+request = """
+GET /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
 
 response = Servy.Handler.handle(request)
 
